d85feb43ffa057d126dafe2062baf02c159f3676
Teaeye <ilya.tomskih@gmail.com>
2021-08-19 12:48:55 +0700

    asdf

--- src/main/java/com/javadeveloperzone/DemoController.java
+++ src/main/java/com/javadeveloperzone/DemoController.java
@@ -1,7 +1,12 @@
 package com.javadeveloperzone;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
-    public static void main(String[] args) {
-        //
+    @RequestMapping(value = "hello")
+    public String hello(){
+        return "Spring boot Gradle Example";
     }
 }
